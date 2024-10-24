4443f6dcdb49eb816bc1d6cb8b93186eb656f618
sbadoni21 <97601162+sbadoni21@users.noreply.github.com>
2024-10-24 13:56:31 +0530

    new

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
+
+app.use(cors());
 const app = express();
 const PORT = 8080;
 
